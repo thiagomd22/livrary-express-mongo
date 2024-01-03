a933a776edfe9f6042a90548dfd3358bfb8f355c
Thiago Meireles <thiago22md@gmail.com>
2024-01-03 11:47:26 -0300

    .env_and_routelivros

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase() {
-  mongoose.connect("mongodb+srv://admin:admin123@cluster0.neoudj4.mongodb.net/livraria?retryWrites=true&w=majority");
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
 
   return mongoose.connection;
 };
